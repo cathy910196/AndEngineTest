557fe94f86f3c93362e30285f04c8c69de76e484
cathy910196 <cathy910196@gmail.com>
2018-04-10 13:33:49 +0800

    extends BaseGameActivity

--- app/src/main/java/mis/ttu/andenginetest/MainActivity.java
+++ app/src/main/java/mis/ttu/andenginetest/MainActivity.java
@@ -3,11 +3,36 @@ package mis.ttu.andenginetest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.entity.scene.Scene;
+import org.andengine.ui.IGameInterface;
+import org.andengine.ui.activity.BaseGameActivity;
+
+public class MainActivity extends BaseGameActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public EngineOptions onCreateEngineOptions() {
+        return null;
+    }
+
+    @Override
+    public void onCreateResources(OnCreateResourcesCallback pOnCreateResourcesCallback) throws Exception {
+
+    }
+
+    @Override
+    public void onCreateScene(OnCreateSceneCallback pOnCreateSceneCallback) throws Exception {
+
+    }
+
+    @Override
+    public void onPopulateScene(Scene pScene, OnPopulateSceneCallback pOnPopulateSceneCallback) throws Exception {
+
     }
 }
